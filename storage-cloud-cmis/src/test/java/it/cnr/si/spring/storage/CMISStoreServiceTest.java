d5ed9cb56530d9d7b834eabe064174699018a860
marco.spasiano <marco.spasiano@cnr.it>
2018-12-21 15:58:06 +0100

    fix test

--- storage-cloud-cmis/src/test/java/it/cnr/si/spring/storage/CMISStoreServiceTest.java
+++ storage-cloud-cmis/src/test/java/it/cnr/si/spring/storage/CMISStoreServiceTest.java
@@ -12,10 +12,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.math.BigInteger;
 import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -68,6 +70,8 @@ public class CMISStoreServiceTest {
 
     @Test
     public void testVersionable() throws IOException {
+        Optional.ofNullable(storeService.getStorageObjectByPath("/test/test.xml"))
+                .ifPresent(storageObject -> storeService.delete(storageObject));
         StorageFile storageFile = new StorageFile(IOUtils.toInputStream(TEXT, Charset.defaultCharset()),
                 MimeTypes.XML.mimetype(),
                 "test.xml");
@@ -79,12 +83,14 @@ public class CMISStoreServiceTest {
                 "/test",
                 true);
         assertEquals(TEXT, IOUtils.toString(storeService.getResource(storageObject),Charset.defaultCharset()));
-        final StorageObject storageObject1 = storeService.updateStream(
+        assertEquals(BigInteger.valueOf(12), storageObject.<BigInteger>getPropertyValue(StoragePropertyNames.CONTENT_STREAM_LENGTH.value()));
+        final StorageObject storageObject1 = storeService.getStorageObjectBykey(storeService.updateStream(
                 storageObject.getKey(),
                 IOUtils.toInputStream(TEXT2, Charset.defaultCharset()),
                 MimeTypes.XML.mimetype()
-        );
+        ).getKey());
         assertEquals(TEXT2, IOUtils.toString(storeService.getResource(storageObject1),Charset.defaultCharset()));
+        assertEquals(BigInteger.valueOf(16), storageObject1.<BigInteger>getPropertyValue(StoragePropertyNames.CONTENT_STREAM_LENGTH.value()));
     }
 
 }
